d6ad8fc443077055555d8568512208aa91c5aacd
rehank <rehanmj23@gmail.com>
2016-06-06 21:35:53 -0500

    A new script to automate a menu item URL update

---
+++ reloadSaveEmailScript.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+driver = webdriver.Firefox()
+
+driver.get("url")
+
+
+#login
+#go to navigation menu items
+#edit Reload save email menu item
+#change to:
+#save
+#go to reload saved email
+#verify url is updated
