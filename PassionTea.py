d57f65042824e16b12c7eb11d78b0df5bada9d9a
rehank <rehanmj23@gmail.com>
2016-06-12 02:23:06 -0500

    automate billing step

--- PassionTea.py
+++ PassionTea.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support.ui import Select
+import time
+from random import randint
 
 
 driver = webdriver.Firefox()
@@ -23,4 +27,42 @@ greenTea.click()
 assert "Check Out" in driver.title
 print "%s" % driver.title
 
+email = driver.find_element_by_xpath("//input[@id='email']")
+email.click()
+email.send_keys("testemail@mailinator.com")
 
+name = driver.find_element_by_xpath("//input[@id='name']")
+name.click()
+name.send_keys("Tom")
+
+address = driver.find_element_by_xpath("//textarea[@id='address']")
+address.click()
+address.send_keys("123 Hallow Dr")
+address.send_keys(Keys.RETURN)
+address.send_keys("98765, Dallas TX")
+
+select = Select(driver.find_element_by_id('card_type'))
+select.select_by_visible_text('American Express')
+
+cardNumber = driver.find_element_by_xpath("//input[@id='card_number']")
+cardNumber.click()
+cardNumber.send_keys("55555555555")
+
+cardHolderName = driver.find_element_by_xpath("//input[@id='cardholder_name']")
+cardHolderName.click()
+cardHolderName.send_keys("Tom Bob")
+
+verifiCode = driver.find_element_by_xpath("//input[@id='verification_code']")
+verifiCode.click()
+verifiCode.send_keys("54321")
+
+placeOrder = driver.find_element_by_xpath("//button[@class='btn btn-primary']")
+placeOrder.click()
+
+letsTalk = driver.find_element_by_xpath("//a[@data-url='let-s-talk-tea.html']")
+letsTalk.click()
+assert "Let's Talk Tea" in driver.title
+print "%s" % driver.title
+
+
+drive.close()
