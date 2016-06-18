d279d70fe87d27515f6748393f9dc50fcd1e5283
Rehan K <RehanK@users.noreply.github.com>
2016-06-18 15:11:13 -0500

    Update alertPopUp.py

--- alertPopUp.py
+++ alertPopUp.py
@@ -9,3 +9,5 @@ driver.get('http://www.seleniumframework.com/Practiceform/')
 alertBox = driver.find_element_by_xpath("//button[@id='alert']").click()
 
 alert = driver.switch_to_alert()
+
+driver.close()
