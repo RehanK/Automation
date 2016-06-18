34e77393186f295c27d2c0eaeef2b12970a767a9
Rehan K <RehanK@users.noreply.github.com>
2016-06-18 15:09:45 -0500

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import Select
-import time
-
-driver = webdriver.Firefox()
-driver.get("www.google.com")
-
-testing
