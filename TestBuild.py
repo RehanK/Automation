81ec00224dede6217a93f9c2e426e0eb990f80c9
RehanK <rehanmj23@gmail.com>
2019-07-16 22:51:28 -0500

    updated

--- TestBuild.py
+++ TestBuild.py
@@ -1,2 +1,4 @@
+print "hey this script is running"
+
 print "Hello, this file is for building my project"
 
--- TestRun.py
+++ TestRun.py
@@ -1,4 +1,4 @@
-//testing script udpated
+//123123123123123123123123123123123123testing script udpated
 
 print "Hello, this is my run file"
 print 9 + 10 
